9f3dd85359c47fa7fa988f0639c3215bcd1eb2c2
ritaVD <ritavicdom@gmail.com>
2025-10-20 10:58:23 +0200

    prove

--- db.php
+++ db.php
@@ -3,7 +3,7 @@ class Connection
 {
     public static function connect()
     {
-        $connect = new mysqli("localhost", "root", "", "Ecommerce");
+        $connect = new mysqli("localhost", "root", "", "E");
         $connect->query("SET NAMES 'utf8'");
         return $connect;
     }
