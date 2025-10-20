4652ec025014e22df850c4efb544313eee30146a
sergioramirezmoron <sergiothd50@gmail.com>
2025-10-20 12:13:19 +0200

    Added ProductRepository

--- models/ProductRepository.php
+++ models/ProductRepository.php
@@ -1,2 +1,70 @@
 <?php
-?>
+
+class ProductRepository
+{
+    public function getAllProducts()
+    {
+        $db = Connection::connect();
+        $q = "SELECT * FROM products";
+        $result = $db->query($q);
+        $products = [];
+        while ($row = $result->fetch_assoc()) {
+            $products[] = new Product(
+                $row['id'],
+                $row['name'],
+                $row['description'],
+                $row['price'],
+                $row['stock'],
+                $row["idCategory"],
+                $row['image']
+            );
+        }
+        $db->close();
+        return $products;
+    }
+
+    public function getProductById($id)
+    {
+        $db = Connection::connect();
+        $q = "SELECT * FROM products WHERE id = $id";
+        $result = $db->query($q);
+        $product = null;
+        if ($row = $result->fetch_assoc()) {
+            $product = new Product(
+                $row['id'],
+                $row['name'],
+                $row['description'],
+                $row['price'],
+                $row['stock'],
+                $row["idCategory"],
+                $row['image']
+            );
+        }
+        $db->close();
+        return $product;
+    }
+
+    public function addProduct($name, $description, $price, $stock, $idCategory, $image)
+    {
+        $db = Connection::connect();
+        $q = "INSERT INTO products VALUES (null, '$name', '$description', $price, $stock, $idCategory, '$image')";
+        $db->query($q);
+        $db->close();
+    }
+
+    public function updateProduct($id, $name, $description, $price, $stock, $idCategory, $image)
+    {
+        $db = Connection::connect();
+        $q = "UPDATE products SET name='$name', description='$description', price=$price, stock=$stock, idCategory=$idCategory, image='$image' WHERE id=$id";
+        $db->query($q);
+        $db->close();
+    }
+
+    public function deleteProduct($id)
+    {
+        $db = Connection::connect();
+        $q = "DELETE FROM products WHERE id=$id";
+        $db->query($q);
+        $db->close();
+    }
+}
