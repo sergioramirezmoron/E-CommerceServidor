64dc03f737d34ee22cfca07687214ba017725d29
sergioramirezmoron <sergiothd50@gmail.com>
2025-10-21 13:10:39 +0200

    delete things

--- controllers/cartController.php
+++ controllers/cartController.php
@@ -17,14 +17,3 @@ if (isset($_POST["finishBuys"])) {
     require_once('views/soldView.phtml');
     exit;
 }
-
-//Ver si el carrito esta pagado
-if (isset($_GET['paid'])) {
-    if (!isset($_SESSION['user']) || !$_SESSION['user']) {
-        header('Location: index.php?c=user&login=1');
-        exit;
-    }
-    $orderId = (int)$_GET['paid'];
-    require_once('views/paidView.phtml');
-    exit;
-}
