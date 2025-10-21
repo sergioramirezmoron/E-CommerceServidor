2d6a8e9875587f03a23945dba3703b0f8f3ed589
sergioramirezmoron <sergiothd50@gmail.com>
2025-10-21 14:22:49 +0200

    delete unusual variables

--- models/OrderRepository.php
+++ models/OrderRepository.php
@@ -7,7 +7,6 @@ class OrderRepository
     public static function getOrderById($id)
     {
         $db = Connection::connect();
-        $id = intval($id);
         $q = "SELECT * FROM orders WHERE id = $id";
         $search = $db->query($q);
         if ($row = $search->fetch_assoc()) {
@@ -21,7 +20,6 @@ class OrderRepository
     public static function getOrdersByUserId($userId)
     {
         $db = Connection::connect();
-        $userId = intval($userId);
         $q = "SELECT * FROM orders WHERE idUser = $userId";
         $search = $db->query($q);
         $orders = [];
@@ -36,10 +34,6 @@ class OrderRepository
     public static function createOrder($status, $date, $idUser, $idCart)
     {
         $db = Connection::connect();
-        $status = $db->real_escape_string($status);
-        $date = $db->real_escape_string($date);
-        $idUser = intval($idUser);
-        $idCart = intval($idCart);
         $q = "INSERT INTO orders VALUES (null, '$status', '$date', $idUser, $idCart)";
         $insert = $db->query($q);
         if ($insert) {
@@ -52,8 +46,6 @@ class OrderRepository
     public static function updateOrder($id, $status)
     {
         $db = Connection::connect();
-        $id = intval($id);
-        $status = $db->real_escape_string($status);
         $q = "UPDATE orders SET status = '$status' WHERE id = $id";
         $update = $db->query($q);
         if ($update) {
@@ -66,7 +58,6 @@ class OrderRepository
     public function deleteOrder($id)
     {
         $db = Connection::connect();
-        $id = intval($id);
         $q = "DELETE FROM orders WHERE id = $id";
         $delete = $db->query($q);
         if ($delete) {
